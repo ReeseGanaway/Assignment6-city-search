4a92a53afb7ebabb3a30d2c80f64a44883c2a6f5
Reese Ganaway <reeseganaway@gmail.com>
2022-01-11 10:05:30 -0500

    Just changed some varibles to match the city theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState} from "react";
 
-function App() {
+
+
+  function App() {
+
+
+  const [cityName, setCityName]=useState("");
+  const [zipList, setZipList]=useState([]);
+
+  /*const fetchData=async ()=>{
+    const result = await fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipCode}`);
+    const data=await result.json();
+    console.log(data);
+    return data;
+    }*/
+
+  const checkEntry = (event) => {
+    
+    if(event.target.value.length===5){
+      setCityName(event.target.value)
+      console.log(cityName)
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${cityName}`).then((response)=>{
+        response.json().then((data)=>{
+          setZipList(data)
+        console.log(zipList)
+        })
+        
+      })
+      .catch((err)=>console.error(err)) 
+      }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="city-search">City Search</header>
+
+      <input type="submit" onChange={checkEntry}></input>
+      
+      {zipList.map((item) => ( 
+                <ul key = { item.RecordNumber } >
+                    <li>State: {item.State}</li>
+                    <li>Location: {item.Location},</li>
+                    <li>Total Wages: {item.TotalWages}</li>
+                    </ul>))}
     </div>
+
   );
 }
+
+
 
 export default App;
