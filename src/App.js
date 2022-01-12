04d750e0249440085170397064060ff9dab34434
Reese Ganaway <reeseganaway@gmail.com>
2022-01-12 05:36:26 -0500

    Code gets user input for the city and prints the zipcodes

--- src/App.js
+++ src/App.js
@@ -6,43 +6,33 @@ import React, {useState} from "react";
   function App() {
 
 
-  const [cityName, setCityName]=useState("");
   const [zipList, setZipList]=useState([]);
+  const[loading, setLoading]=useState(false);
 
-  /*const fetchData=async ()=>{
-    const result = await fetch(`http://ctp-zip-api.herokuapp.com/zip/${zipCode}`);
-    const data=await result.json();
-    console.log(data);
-    return data;
-    }*/
+  
 
   const checkEntry = (event) => {
     
-    if(event.target.value.length===5){
-      setCityName(event.target.value)
-      console.log(cityName)
-      fetch(`http://ctp-zip-api.herokuapp.com/zip/${cityName}`).then((response)=>{
+      fetch(`http://ctp-zip-api.herokuapp.com/city/${event.target.value.toUpperCase()}`).then((response)=>{
         response.json().then((data)=>{
           setZipList(data)
-        console.log(zipList)
+          setLoading(true)
+          //console.log(loading)
+        //console.log(zipList)
         })
         
       })
       .catch((err)=>console.error(err)) 
       }
-  }
+
+      console.log(JSON.stringify(zipList))
+  
   return (
     <div className="App">
       <header className="city-search">City Search</header>
 
-      <input type="submit" onChange={checkEntry}></input>
-      
-      {zipList.map((item) => ( 
-                <ul key = { item.RecordNumber } >
-                    <li>State: {item.State}</li>
-                    <li>Location: {item.Location},</li>
-                    <li>Total Wages: {item.TotalWages}</li>
-                    </ul>))}
+      <input type="text" onChange={checkEntry}></input>
+      <div id="ZipArray">{JSON.stringify(zipList)}</div>
     </div>
 
   );
